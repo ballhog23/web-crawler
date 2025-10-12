8e5f5adc990ace7bf57214562b76de5be0f85b2b
caleb <calebpirkle2@gmail.com>
2025-10-11 22:39:23 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import { extractPageData } from "./crawl.js";
 
 async function main() {
     const args = process.argv.slice(2);
-    if (args.length < 1 || args.length > 1) {
-        console.error('usage: npm run start <page_url>');
+    if (args.length !== 1) {
+        console.error("usage: npm run start <page_url>");
         process.exit(1);
     }
-
-    const url = args[0];
-    // extractPageData()
+    const baseURL = args[0];
+    console.log(`starting crawl of: ${baseURL}...`);
+    process.exit(0);
 }
+
+main();
 
 main();
